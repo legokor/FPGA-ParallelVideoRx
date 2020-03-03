4668f8efc5f674ee0778475c65be3cb97b6c0a0e
JErdelyi <janka.erdelyi@gmail.com>
2020-03-03 20:34:32 +0100

    Added empty camera interface

---
+++ ov5642_interface.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module ov5642_interface(
+    input pclk,
+    input xclk,
+    input href,
+    input vsync,
+    input [9:0] data,
+    output pwdn
+    );
+    
+    
+endmodule
