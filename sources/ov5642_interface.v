98c6d24c97163280a8e6002844fd50a4fe5685b2
ksstms <ksstms96@gmail.com>
2020-10-01 12:34:58 +0200

    fixed camera interface ports

--- sources/ov5642_interface.v
+++ sources/ov5642_interface.v
@@ -2,12 +2,16 @@
 
 module ov5642_interface(
     input pclk,
-    input xclk,
+    input [7:0] din,
     input href,
     input vsync,
-    input [9:0] data,
-    output pwdn
-    );
-    
-    
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TDATA" *)
+    output [7:0] tdata,
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TLAST" *)
+    output tlast,
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TVALID" *)
+    output tvalid
+);
+
 endmodule
