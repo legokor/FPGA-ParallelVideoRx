e960cbc9011384fce326a02b9bd410704aad16d4
ksstms <ksstms96@gmail.com>
2021-03-28 20:12:47 +0200

    added vsync and href polarity parameters (#1)

--- sources/dvp_receiver.v
+++ sources/dvp_receiver.v
@@ -1,10 +1,13 @@
 `timescale 1ns / 1ps
 
-module dvp_receiver (
+module dvp_receiver #(
+    parameter VSYNC_ACTIVE_HIGH = 0,
+    parameter HREF_ACTIVE_HIGH = 1
+) (
     input pclk,
     input [7:0] din,
-    input href,
-    input vsync,
+    input href_in,
+    input vsync_in,
 
     (* X_INTERFACE_PARAMETER = "CLK_DOMAIN pclk" *)
     (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TDATA" *)
@@ -16,6 +19,13 @@ module dvp_receiver (
     (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TUSER" *)
     output tuser
 );
+
+/*
+ * Invert HREF and VSYNC if necessary
+ */
+wire href, vsync;
+assign href  = HREF_ACTIVE_HIGH  ? href_in  : ~href_in;
+assign vsync = VSYNC_ACTIVE_HIGH ? vsync_in : ~vsync_in;
 
 /*
  * Rename clock
